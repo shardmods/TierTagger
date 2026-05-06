5bcae8fe85819b561fb8f174599870c00cfdbb43
Lyosha <public@lyosha.sh>
2026-05-06 08:58:05 -0400

    im stupid i know

--- common/src/main/java/net/uku3lig/tiertagger/mixin/MixinPlayer.java
+++ common/src/main/java/net/uku3lig/tiertagger/mixin/MixinPlayer.java
@@ -13,7 +13,7 @@ public class MixinPlayer {
     public Component prependTier(Component original) {
         if (TierTagger.getManager().getConfig().isEnabled()) {
             Player self = (Player) (Object) this;
-            return TierTagger.appendTier(self.getUUID(), original);
+            return TierTagger.appendTier(self.getUUID(), original, self.getName());
         } else {
             return original;
         }
